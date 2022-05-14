ed86dcdfcdc86b5d49ed820ceb85d2adebe8840c
Ayon <ayonjodder177@gmail.com>
2022-05-15 01:56:05 +0600

    Modal done

--- src/Appointment/Appoingment.js
+++ src/Appointment/Appoingment.js
@@ -2,24 +2,26 @@ import React, { useState } from 'react';
 import heroImage from '../assets/images/chair.png'
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
-import { format } from 'date-fns';
+import AvailableAppointemnt from './AvailableAppointemnt';
 
 const Appoingment = () => {
     const [date, setDate] = useState(new Date())
     return (
-        <div className="hero min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={heroImage} className="md:w-1/2 rounded-lg shadow-2xl" alt='' />
-                <div>
-                    <DayPicker
-                        mode="single"
-                        selected={date}
-                        onSelect={setDate}
-                    />
-                    <p>You picked {format(date, 'PP')}.</p>;
+        <>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={heroImage} className="md:w-1/2 rounded-lg shadow-2xl" alt='' />
+                    <div>
+                        <DayPicker
+                            mode="single"
+                            selected={date}
+                            onSelect={setDate}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+            <AvailableAppointemnt date={date}></AvailableAppointemnt>
+        </>
     );
 };
 
---
+++ src/Appointment/AvailableAppointemnt.js
@@ -0,0 +1,29 @@
+import { format } from 'date-fns';
+import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
+import ServiceCard from './ServiceCard';
+
+const AvailableAppointemnt = ({ date }) => {
+    const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState({})
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+    return (
+        <div>
+            <p className='text-center text-secondary text-lg'>Available Appointment on {format(date, 'PP')}</p>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <ServiceCard setTreatment={setTreatment} key={service._id} service={service}></ServiceCard>)
+                }
+            </div>
+            {
+                treatment && <BookingModal treatment={treatment}></BookingModal>
+            }
+        </div>
+    );
+};
+
+export default AvailableAppointemnt;
